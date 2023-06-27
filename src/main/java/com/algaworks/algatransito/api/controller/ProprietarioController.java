7bd27545f118ce56a8793b82412070d0fd19f08d
Wellington Felix <wffelixy@gmail.com>
2023-06-27 13:31:18 -0300

    Implementando endpoint de busca de recurso

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,8 +1,10 @@
 package com.algaworks.algatransito.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,15 +22,30 @@ public class ProprietarioController {
 
 	@GetMapping
 	public List<Proprietario> listar() {
-
 		return proprietarioRepository.findAll();
-
 	}
 
-	@GetMapping("/{nome}")
+	@GetMapping("/nome/{nome}")
 	public List<Proprietario> buscarPeloNome(@PathVariable String nome) {
 		return proprietarioRepository.findByNomeContaining(nome);
 
+	}
+
+	@GetMapping("/{proprietarioId}")
+	public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
+		
+
+		//Recurso via lambda
+		return proprietarioRepository.findById(proprietarioId)
+				.map(proprietario -> ResponseEntity.ok(proprietario))
+				.orElse(ResponseEntity.notFound().build());
+		
+		//TODO Recurso sem usar Lambda
+		//		if (proprietario.isPresent()) {
+//			return ResponseEntity.ok(proprietario.get());
+//		}
+//		
+//		return ResponseEntity.notFound().build();
 	}
 
 }
