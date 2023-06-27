8b7a5efabf788dada1a4a9f08524dba9772a8a09
Wellington Felix <wffelixy@gmail.com>
2023-06-27 13:02:43 -0300

    Implementando Query Methods no repositório

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,30 +1,34 @@
 package com.algaworks.algatransito.api.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
 import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-
 @RestController
+@RequestMapping("/proprietarios")
 public class ProprietarioController {
-	
+
 	@Autowired
 	private ProprietarioRepository proprietarioRepository;
-	
-	@GetMapping("/proprietarios")
-	public List<Proprietario> listar(){
-		
+
+	@GetMapping
+	public List<Proprietario> listar() {
+
 		return proprietarioRepository.findAll();
-		
-		
+
+	}
+
+	@GetMapping("/{nome}")
+	public List<Proprietario> buscarPeloNome(@PathVariable String nome) {
+		return proprietarioRepository.findByNomeContaining(nome);
+
 	}
 
 }
--- src/main/java/com/algaworks/algatransito/domain/repository/ProprietarioRepository.java
+++ src/main/java/com/algaworks/algatransito/domain/repository/ProprietarioRepository.java
@@ -1,5 +1,7 @@
 package com.algaworks.algatransito.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.algaworks.algatransito.domain.model.Proprietario;
 
 @Repository
 public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
+	
+	List<Proprietario> 	findByNomeContaining(String nome);
 
 }
