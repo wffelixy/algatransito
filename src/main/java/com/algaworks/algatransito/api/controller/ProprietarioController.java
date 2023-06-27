060f0d63b5b3db17d06203a98d7c28e047c1f4c0
Wellington Felix <wffelixy@gmail.com>
2023-06-27 14:25:28 -0300

    Implementando endpoint de exclusão de recurso

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -54,6 +56,32 @@ public class ProprietarioController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Proprietario cadastrar(@RequestBody Proprietario proprietario) {
 		return proprietarioRepository.save(proprietario);
+	}
+
+	@PutMapping("/{proprietarioId}")
+	public ResponseEntity<Proprietario> atualizar(@PathVariable Long proprietarioId,
+			@RequestBody Proprietario proprietario) {
+
+		if (!proprietarioRepository.existsById(proprietarioId)) {
+			return ResponseEntity.notFound().build();
+		}
+
+		proprietario.setId(proprietarioId);
+
+		Proprietario proprietarioAtualizado = proprietarioRepository.save(proprietario);
+
+		return ResponseEntity.ok(proprietarioAtualizado);
+	}
+	
+	@DeleteMapping("/{proprietarioId}")
+	public ResponseEntity<Void> remover(@PathVariable Long proprietarioId) {
+		if (!proprietarioRepository.existsById(proprietarioId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		proprietarioRepository.deleteById(proprietarioId);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
